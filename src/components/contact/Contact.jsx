a880fe0bd6d838655358756b63db1f7373107da1
nidhinagpure <nagpurenidhi8@gmail.com>
2025-07-04 18:38:16 +0530

    contact from init

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,52 +1,64 @@
 import './../contact/Contact.css';
+import Heading from './../../components/headingname/Heading';
 
 import Email from './../../assets/footer/email.png';
-import Contact from './../../assets/footer/Phone-call.png';
-import Location from './../../assets/profile/Location.png';
+import PhoneIcon from './../../assets/footer/Phone-call.png'; // ✅ renamed
+import Location from './../../assets/project/Location.png';
+import Linkdin from './../../assets/footer/Linkdin.png';
 
-
-
-
-
-
-
-function contactDeatils(){
-  return(
-    <>
-    <div>
-      <div>
-        <h1>Let's Talk</h1>
-        <p>I'm currently avaliable to take on 
-          new projects, so feel free to send 
-          me a message about anything that 
-          you want me to work on. You can 
-          contact anytime.</p>
-      </div>
-         <div>
-           <h1><img src={Email}/>nidhinagpure21@gmail.com</h1>
-           <h1><img src={Contact}/>91+ 9284011752</h1>
-           <h1><img src={Location}/>Nagpur, Maharashtra</h1>
-
-
-         </div>
-        
-    </div>
-    
-    </>
-  )
-}
-
-
-
-export function Contact(){
+function ContactDeatils() {
   return (
     <>
+           <Heading heading="Get In Touch"/>
 
-       
-    
+      <div className='conatct-content'>
+        <div className='contact-detail'>
+          <h1>Let's Talk</h1>
+          <p>
+            I'm currently available to take on new projects, so feel free to send
+            me a message about anything that you want me to work on.
+            You can contact me anytime.
+          </p>
+          <div className='social-media'>
+            <p><img src={Email} alt="Email" /> nidhinagpure21@gmail.com</p>
+            <p><img src={PhoneIcon} alt="Phone" /> +91 9284011752</p>
+            <p><img src={Location} alt="Location" /> Nagpur, Maharashtra</p>
+            <a href='https://www.linkedin.com/in/nidhi-nagpure-160110283/'
+              target='_blank' style={{ textDecoration: "none", color: 'black' }}>
+              <p><img src={Linkdin} />Linkdin</p></a>
+          </div>
+        </div>
+
+        <div>
+          <form class="contact-form">
+            
+              <label for="name">Your Name</label>
+              <input type="text" id="name" placeholder="Enter your name" required />
+           
+             
+              <label for="email">Your Email</label>
+              <input type="email" id="email" placeholder="Enter your email" required />
+            
+              <label for="message">Write your message here</label>
+              <textarea id="message" placeholder="Enter your message" rows="10" required></textarea>
+              <button className='submit-btn'>Let's Go</button>
+
+          </form>
+        </div>
+
+      </div>
     </>
+  );
+}
 
-  )
-};
+export function Contact() {
+  return (
+    <>
+      <div>
+        <ContactDeatils />
+      </div>
+    </>
+  );
+}
 
 export default Contact;
