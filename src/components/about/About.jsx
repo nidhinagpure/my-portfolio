149d235b073c669f7616f3a64d654608cf3a6042
nidhinagpure <nagpurenidhi8@gmail.com>
2025-07-04 20:01:58 +0530

    about export in about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,32 @@
+
 import './../about/About.css';
+
+import Heading from './../headingname/Heading';
+import Profileimg from './../../assets/Profile-img.jpg';
 
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <Heading heading ="About Me"/>
+
+      <div className='home-container'>
+
+        <div className='profile-container'>
+          <img src={Profileimg} />
+
+        </div>
+
+        <div className='home-info'>
+          <h1>Hello,</h1>
+          <h1>I'm Nidhi Nagpure </h1>
+          <h2>I'm a <span> Full Stack Developer </span></h2>
+          <p>Bridging the gap between design and functionality, I specialize in creating smart, scalable,
+            and user-friendly web applications from front to back .</p>
+          <div className='btn-resume'>
+            <a href="/resume.pdf" download className="btn-outline">My Resume</a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,7 @@
 import './../home/Home.css';
 
+import Navbar from './../navbar/Navbar';
+import About from './../../components/about/About';
 import { Skill } from './../../components/skill/skill';
 import { Service } from './../../components/service/Service';
 import { Project } from './../../components/project/Project';
@@ -7,34 +9,13 @@ import { Education } from './../../components/education/Education';
 import { Contact } from './../../components/contact/Contact';
 import Footer from './../../components/footer/Footer';
 
-import Navbar from './../navbar/Navbar';
-import Heading from './../headingname/Heading';
-import Profileimg from './../../assets/Profile-img.jpg';
-
 const Home = () => {
   return (
     <div>
       <Navbar />
       <div className='line-hr'></div>
-      <Heading heading ="About Me"/>
-
-      <div className='home-container'>
-
-        <div className='profile-container'>
-          <img src={Profileimg} />
-
-        </div>
-
-        <div className='home-info'>
-          <h1>Hello,</h1>
-          <h1>I'm Nidhi Nagpure </h1>
-          <h2>I'm a <span> Full Stack Developer </span></h2>
-          <p>Bridging the gap between design and functionality, I specialize in creating smart, scalable,
-            and user-friendly web applications from front to back .</p>
-          <div className='btn-resume'>
-            <a href="/resume.pdf" download className="btn-outline">My Resume</a>
-          </div>
-        </div>
+      <div>
+        <About />
       </div>
 
       <div>
